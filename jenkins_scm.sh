a4f1e2d75deaf9b796a541ea7c8b2662f8e1fe7a
Darrell Hamilton <darrell.noice@gmail.com>
2011-10-31 09:44:09 -0700

    Brought in changes from work

--- jenkins_scm.sh
+++ jenkins_scm.sh
@@ -4,12 +4,24 @@
 cd $JENKINS_HOME
 
 # Add general configurations, job configurations, and user content
-git add *.xml jobs/*/*.xml users/*/config.xml userContent/*
+git add -- *.xml jobs/*/*.xml userContent/*
+
+# only add user configurations if they exist
+if [ -d users ]; then
+    user_configs=`ls users/*/config.xml`
+
+    if [ -n "$user_configs" ]; then
+        git add $user_configs
+    fi
+fi
 
 # mark as deleted anything that's been, well, deleted
-git rm `git status | grep "deleted" | awk '{print $3}'`
+to_remove=`git status | grep "deleted" | awk '{print $3}'`
+
+if [ -n "$to_remove" ]; then
+    git rm --ignore-unmatch $to_remove
+fi
 
 git commit -m "Automated Jenkins commit"
 
 git push -q
-
